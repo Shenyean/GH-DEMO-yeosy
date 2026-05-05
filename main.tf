93375abfe111b8071ea364c6e7654122508c4360
Yeosy <yeosy@live.com>
2026-05-06 00:10:41 +0800

    terraform action demo v1.0

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "sctp-ce12-tfstate-bucket"
+    key    = "test/yeosy-terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-github-actions-test-bucket"
+
+  tags = {
+    Name        = "yeosy-github-actions-test-bucket"
+    Environment = "test"
+    ManagedBy   = "terraform"
+  }
+}
+
+output "bucket_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.test_bucket.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN of the created S3 bucket"
+  value       = aws_s3_bucket.test_bucket.arn
+}
